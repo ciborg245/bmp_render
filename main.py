23d0b4c3dbd9709ff8d4dc7bd2f1d7f1e718f421
Alejandro <alejandrochgu@hotmail.com>
2019-02-25 16:57:49 -0600

    sr1 finished and t1

---
+++ main.py
@@ -0,0 +1,152 @@
+from render import Render
+from random import uniform, randint
+from math import floor
+
+ren = Render()
+
+####### Item 1
+# ren.glCreateWindow(600, 400)
+# ren.glClearColor(0, 0, 0)
+# ren.glViewPort(0, 0, 600, 400)
+# ren.glVertex(uniform(-1, 1), uniform(-1, 1))
+# ren.glColor(1, 1, 1)
+#
+# ren.glFinish("item1")
+
+###### --------
+
+# ##### Item 2
+# ren.glCreateWindow(500, 500)
+# ren.glClearColor(0, 0, 0)
+# ren.glViewPort(0, 0, 499, 499)
+# ren.glColor(1, 1, 1)
+# ren.glVertex(-1, 1)
+# ren.glVertex(1, 1)
+# ren.glVertex(1, -1)
+# ren.glVertex(-1, -1)
+#
+# ren.glFinish('item2')
+# ##### --------
+
+###### Item 3
+width = 500
+height = 500
+ren.glCreateWindow(width, height)
+ren.glClearColor(0, 0, 0)
+
+xVP = floor(height/2 - 50)
+yVP = floor(width/2 - 50)
+
+ren.glColor(1, 1, 1)
+
+ren.glViewPort(xVP, yVP, 100, 100)
+i = 2/100
+cont = -1
+while (cont <= 1):
+    ren.glVertex(1, cont)
+    ren.glVertex(cont, 1)
+    ren.glVertex(-1, cont)
+    ren.glVertex(cont, -1)
+    cont += i
+
+# xVP = floor(height/2 - 50)
+# yVP = floor(width/2 - 50)
+# ren.glViewPort(xVP, yVP, 150, 50)
+
+ren.glFinish('item3')
+
+###### ------
+
+###### Item 4
+
+width = 500
+height = 500
+ren.glCreateWindow(width, height)
+ren.glClearColor(0, 0, 0)
+
+xVP = 0
+yVP = 0
+
+ren.glColor(1, 1, 1)
+
+ren.glViewPort(5, 5, 490, 490)
+i = 2/486
+cont = -1
+while (cont <= 1):
+    ren.glVertex(1, cont)
+    ren.glVertex(cont, 1)
+    ren.glVertex(-1, cont)
+    ren.glVertex(cont, -1)
+    cont += i
+
+ren.glFinish('item4')
+
+###### ------
+
+###### Item 5
+
+width = 500
+height = 500
+ren.glCreateWindow(width, height)
+ren.glClearColor(0, 0, 0)
+
+ren.glColor(1, 1, 1)
+
+ren.glViewPort(0, 0, 499, 499)
+i = 2/500
+cont = -1
+while (cont <= 1):
+    ren.glVertex(cont, cont)
+    cont += i
+
+ren.glFinish('item5')
+
+###### ------
+
+###### Item 6
+
+width = 500
+height = 500
+ren.glCreateWindow(width, height)
+ren.glClearColor(0, 0, 0)
+
+ren.glColor(1, 1, 1)
+
+ren.glViewPort(0, 0, 499, 499)
+s = 2/500
+i = -1
+while (i <= 1):
+    j = -1
+    while (j <= 1):
+        if randint(0,1) == 0:
+            ren.glVertex(i, j)
+        j += s
+    i += s
+
+ren.glFinish('item6')
+
+###### ------
+
+###### Item 7
+
+width = 500
+height = 500
+ren.glCreateWindow(width, height)
+ren.glClearColor(0, 0, 0)
+
+ren.glColor(1, 1, 1)
+
+ren.glViewPort(0, 0, 499, 499)
+s = 2/500
+i = -1
+while (i <= 1):
+    j = -1
+    while (j <= 1):
+        if randint(0,1) == 0:
+            ren.glVertex(i, j)
+        j += s
+    i += s
+
+ren.glFinish('item7')
+
+###### ------
---
+++ render.py
@@ -0,0 +1,51 @@
+from math import trunc
+from bitmap import Bitmap, color
+
+class Render(object):
+    def __init__(self, width = None, height = None):
+        self.x = 0
+        self.y = 0
+        self.view = {}
+        self.color = color(255, 255, 255)
+
+        if (width == None and height == None):
+            self.bmp = None
+            self.width = width
+            self.height = height
+        else:
+            self.glCreateWindow(width, height)
+
+    def glInit(self):
+        pass
+
+    def glCreateWindow(self, width, height):
+        self.bmp = Bitmap(width, height)
+        self.width = width
+        self.height = height
+
+    def glViewPort(self, x, y, width, height):
+        self.view['x'] = x
+        self.view['y'] = y
+        self.view['width'] = width-1
+        self.view['height'] = height-1
+
+    def glClear(self):
+        self.bmp.clear()
+
+    def glClearColor(self, r, g, b):
+        for i in range(self.height):
+            for j in range(self.width):
+                self.bmp.point(i, j, color(trunc(r * 255), trunc(g * 255), trunc(b * 255)))
+
+    def glVertex(self, x, y):
+        self.x = trunc(self.view['x'] + (self.view['height'] / 2) * (x + 1))
+        self.y = trunc(self.view['y'] + (self.view['width'] / 2) * (y + 1))
+
+        self.bmp.point(self.x, self.y, self.color)
+
+    def glColor(self, r, g, b):
+        self.color = color(trunc(r * 255), trunc(g * 255), trunc(b * 255))
+        # self.bmp.point(self.x, self.y, color(trunc(r * 255), trunc(g * 255), trunc(b * 255)))
+
+    def glFinish(self, name = 'out'):
+        self.bmp.write(name+'.bmp')
