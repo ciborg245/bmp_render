f77af103cd1bbc2e290a30bb16d628c02f9fe431
Alejandro <alejandrochgu@hotmail.com>
2019-04-17 12:26:11 -0600

    SR4 changes, SR5 added

---
+++ SR5/obj.py
@@ -0,0 +1,17 @@
+def processObj(file):
+    vertices = []
+    faces = []
+    normals = []
+
+    cont = 0
+    for line in file:
+        lineArr = line.rstrip().split(' ')
+
+        if lineArr[0] == 'v':
+            vertices.append(list(map(float, lineArr[1:])))
+        elif lineArr[0] == 'f':
+            faces.append([list(map(int, f.split('/'))) for f in lineArr[1:]])
+        elif lineArr[0] == 'vn':
+            normals.append(list(map(float, lineArr[1:])))
+
+    return faces, vertices
